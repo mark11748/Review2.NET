32dc8be78d940549282eb3b3c318af6c8d135fd9
Mark Woodward <markwood117@gmail.com>
2018-01-19 18:24:45 -0800

    setup model structure

---
+++ Controllers/ReviewsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Review2NET.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateReview(int productId)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace Review2_NET.Models
+{
+    [Table("Review")]
+    public class Review
+    {
+        [Key]
+        public int ReviewId   { get; set; }
+        public int ProductId  { get; set; }
+        public string Author  { get; set; }
+        public string Comment { get; set; }
+    }
+}
